6f3e936012cacc7d8a8d9e68417b5ef6acff45b4
Jenny <yjhong1004@gmail.com>
2019-02-06 15:01:45 -0800

    trying to get alert messages working/added bootstrap

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'stripe'
 
 gem 'pundit'
 
+gem 'bootstrap'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- app/controllers/charges_downgrade_controller.rb
+++ app/controllers/charges_downgrade_controller.rb
@@ -3,7 +3,7 @@ def downgrade
   flash[:alert] = "Your wikis will become public if you downgrade."
   current_user.wikis.each {|wiki| wiki.update_attribute(:private, false) }
   current_user.update_attribute(:role, 'standard')
-  flash[:alert] = "#{current_user.email} have downgraded to the standard membership."
+  flash[:notice] = "#{current_user.email} have downgraded to the standard membership."
   redirect_to new_charge_path
 end
 end

dba2db24fc9f3faded924fe84999eb32ea05ebc8
Jenny <yjhong1004@gmail.com>
2019-03-06 13:32:31 -0800

    collaborator model spec added

--- spec/models/collaborator_spec.rb
+++ spec/models/collaborator_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Collaborator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   let(:user) { create(:user) }
+   let(:wiki) { create(:wiki) }
+   let(:collaborator) { Collaborator.create!(wiki: wiki, user: user) }
+
+   it { is_expected.to belong_to(:wiki) }
+   it { is_expected.to belong_to(:user) }
 end
